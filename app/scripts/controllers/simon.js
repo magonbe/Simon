9aafd4cd9eeffe15fa1114ea9c3f273bd1a9a62a
magonbe <majogonbe@gmail.com>
2016-01-26 20:45:31 +0100

    primeros cambios

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -8,47 +8,17 @@
  * Controller of the simonApp
  */
 angular.module('simonApp')
-  .controller('SimonCtrl', function () {
-   /*Desde aqui...*/
-  $(document).ready(function(){
-    
-    $("#parrafo").click(function(){
-      $("#titulo").text("rojo");
-      $("p").hide();
-    })
-    
-    $("#Verde").click(function(){
-      $("#Verde").text("Verde");
-      $("#Azul").text("");
-      $("#Amarillo").text("");
-      $("#Rojo").text("");
-    })
-    
-    $("#Rojo").click(function(){
-      $("#Rojo").text("Rojo");
-      $("#Azul").text("");
-      $("#Amarillo").text("");
-      $("#Verde").text("");
-      
-    })
-    
-      $("#Azul").click(function(){
-      $("#Azul").text("Azul");
-      $("#Verde").text("");
-      $("#Amarillo").text("");
-      $("#Rojo").text("");
-    })
-    
-      $("#Amarillo").click(function(){
-      $("#Amarillo").text("Amarillo");
-      $("#Verde").text("");
-      $("#Azul").text("");
-      $("#Rojo").text("");
-    })
-    
-    })/*Hasta aqui,sería con la función jquery*/
-  
-    function envia(valor){
-      document.getElementById('titulo').innerHTML= valor;
-    }
-  });
+  .controller('SimonCtrl', function ($scope) {
+   		
+   		$scope.colores=[];
+   		
+   		$scope.titulo='simon';
+
+  	$scope.play= function(color){
+  		$scope.colores. push(color);
+  		
+  		}
+  	$scope.borrar= function(){
+  			$scope.colores=[];
+  		}
+});
