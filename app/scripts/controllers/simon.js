aa3042123105144ec219d13d2e108017b1cc9e20
magonbe <majogonbe@gmail.com>
2016-01-28 20:01:20 +0100

    2º cambio

--- app/scripts/controllers/simon.js
+++ app/scripts/controllers/simon.js
@@ -13,12 +13,42 @@ angular.module('simonApp')
    		$scope.colores=[];
    		
    		$scope.titulo='simon';
+      $scope.acumulador= 0;
+      $scope.acumuladorverde= 0;
+      $scope.acumuladorazul= 0;
+      $scope.acumuladoramarillo= 0;
+      $scope.acumuladorrojo= 0;
 
   	$scope.play= function(color){
-  		$scope.colores. push(color);
+      if (color=='verde') {
+            $scope.acumuladorverde = $scope.acumuladorverde + 1;
+      }
+     
+
+
+      if (color=='azul') {
+            $scope.acumuladorazul = $scope.acumuladorazul + 1;
+      }
+      
+      
+      
+
+      if (color=='amarillo') {
+            $scope.acumuladoramarillo = $scope.acumuladoramarillo + 1;
+      }
+    
+      
+
+      if (color=='rojo') {
+            $scope.acumuladorrojo = $scope.acumuladorrojo + 1;
+      }
+    
+      $scope.colores. push(color);
   		
   		}
   	$scope.borrar= function(){
-  			$scope.colores=[];
-  		}
+        $scope.colores=[];
+  			$scope.acumulador=(0)
+
+      }
 });
